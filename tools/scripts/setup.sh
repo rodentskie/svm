70828f2e699a4df9ede6fffcd676f92595b4a020
Rodney Lingganay <49242131+rodentskie@users.noreply.github.com>
2026-02-22 17:32:08 +0800

    Feat/one time script (#12)
    * feat:  docker students
    
    * feat: scripts
    
    * feat: logout
    
    * chore: readme

---
+++ tools/scripts/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# cleanup docker environment
+docker rm -f $(docker ps -aq) || true
+echo "y" | docker system prune -a && echo "y" | docker volume prune -a && docker system df
+
+# build
+yarn run build
+
+# build docker image
+yarn run docker
+
+# run compose file
+docker-compose -f _docker/docker-compose.yml up -d
