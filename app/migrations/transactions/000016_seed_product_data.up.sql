063e81f52a498dc243561f178d817789294f5990
rodentskie <rodentskie@gmail.com>
2026-02-28 16:36:25 +0800

    fix: seed data

--- app/migrations/transactions/000016_seed_product_data.up.sql
+++ app/migrations/transactions/000016_seed_product_data.up.sql
@@ -10,6 +10,18 @@ INSERT INTO
     )
 VALUES
     ('Rexona', 'REX', 'A-1', 150.00, 4, 2, 1),
+    ('Dove', 'DOV', 'B-1', 160.00, 4, 2, 1),
+    ('DeoNat', 'DEO', 'C-1', 170.00, 4, 2, 1),
+    ('Nivea', 'NIV', 'D-1', 180.00, 4, 2, 1),
     ('Care free', 'CF', 'A-2', 30.00, 4, 2, 2),
+    ('Sanicare', 'SAN', 'B-2', 30.00, 4, 2, 2),
+    ('Kleenex', 'KLE', 'C-2', 30.00, 4, 2, 2),
+    ('Sofeel', 'SOF', 'D-2', 30.00, 4, 2, 2),
     ('Sister', 'SIS', 'A-3', 30.00, 4, 2, 3),
-    ('Dentiste', 'COL', 'A-4', 100, 4, 2, 4);
+    ('Modess', 'MOD', 'B-3', 30.00, 4, 2, 3),
+    ('Whisper', 'WHI', 'C-3', 30.00, 4, 2, 3),
+    ('Charmee', 'CHA', 'D-3', 30.00, 4, 2, 3),
+    ('Dentiste', 'DEN', 'A-4', 100, 4, 2, 4),
+    ('Ortho Kit', 'ORT', 'B-4', 100, 4, 2, 4),
+    ('Colgate', 'COL', 'C-4', 100, 4, 2, 4),
+    ('Sensodyne', 'SEN', 'D-4', 100, 4, 2, 4);
